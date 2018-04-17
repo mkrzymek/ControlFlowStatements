2bf1863c74bf12eddca55a5f112b5ad4fa2a55f5
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-04-17 14:47:38 +0200

    switch statement

--- src/com/timbuchalka/Main.java
+++ src/com/timbuchalka/Main.java
@@ -3,6 +3,30 @@ package com.timbuchalka;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+//        switchStatement();
+        
+    }
+
+    public static void switchStatement() {
+        int switchValue = 1;
+
+        switch (switchValue) {
+            case 1:
+                System.out.println("Value was 1");
+                break;
+
+            case 2:
+                System.out.println("Value was 2");
+                break;
+
+            case 3:case 4:case 5:
+                System.out.println("Value was 3 or 4 or 5");
+                break;
+
+            default:
+                System.out.println("Was not 1 or 2");
+                break;
+        }
     }
 }
